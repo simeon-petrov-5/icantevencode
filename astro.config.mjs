a57112865a999db134890ee101e90a4ee0510196
Simeon Petrov <petrov.v.simeon@gmail.com>
2022-11-03 00:48:36 +0200

    feat: Sitemap and compression plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  site: 'https://icantevencode.com',
+  integrations: [vue(), sitemap(), compress()]
 });
