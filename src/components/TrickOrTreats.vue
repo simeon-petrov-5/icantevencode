3ba936c2843fe31be59f552f1b1d1cd563e0ed33
Simeon Petrov <petrov.v.simeon@gmail.com>
2023-04-23 18:49:08 +0300

    chore: Disable trick component for now

--- src/components/TrickOrTreats.vue
+++ src/components/TrickOrTreats.vue
@@ -1,35 +1,37 @@
-<script>
-  const catchTheKonami = () => {
-    const pattern = [
-      "ArrowUp",
-      "ArrowUp",
-      "ArrowDown",
-      "ArrowDown",
-      "ArrowLeft",
-      "ArrowRight",
-      "ArrowLeft",
-      "ArrowRight",
-      "b",
-      "a",
-    ];
-    let current = 0;
+<script setup lang="ts">
+const catchTheKonami = () => {
+  const pattern = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a",
+  ];
+  let current = 0;
 
-    const onKeydown = function (event: any) {
-      if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
-        current = 0;
-        return;
-      }
+  const onKeydown = function (event: any) {
+    if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+      current = 0;
+      return;
+    }
 
-      current++;
+    current++;
 
-      if (pattern.length === current) {
-        current = 0;
-        window.alert("You found it!");
-      }
-    };
-
-    document.addEventListener("keydown", onKeydown, false);
+    if (pattern.length === current) {
+      current = 0;
+      window.alert("You found it!");
+    }
   };
+
+  document.addEventListener("keydown", onKeydown, false);
+};
+
+if (document) {
   console.log(`
 ░▀█▀░░░█▀▄░█▀█░█▀█░▀░▀█▀░░░█░█░█▀█░█▀█░█░█░░░░░
 ░░█░░░░█░█░█░█░█░█░░░░█░░░░█▀▄░█░█░█░█░█▄█░░░░░
@@ -39,6 +41,6 @@
 ░░█░░░░█░█░░░█░█░█▀█░█▀▄░░█░░█░█░█░█░░░░█░░█▀█░░█░░▀▀█░░░█░█░█▀▀░░░█▀█░▀▀█░░░░█░░░░█░█░█░█░░░
 ░▀▀▀░░░▀░▀░░░▀░▀░▀░▀░▀░▀░▀▀▀░▀░▀░▀▀▀░░░░▀░░▀░▀░▀▀▀░▀▀▀░░░▀▀▀░▀░░░░░▀░▀░▀▀▀░░░▀▀▀░░░▀▀▀░▀▀▀░▀░
 `);
-
-catchTheKonami();
+  catchTheKonami();
+}
 </script>
